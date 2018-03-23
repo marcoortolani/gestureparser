c420cf0fac4fedcf26754133a40f7aafc8490190
Marco Ortolani <marco.ortolani@unipa.it>
2018-03-23 15:58:30 +0100

    Added script for 'manual compilation'

---
+++ compila.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BINDIR=bin
+
+if [ ! -d "$BINDIR" ]; then
+  mkdir $BINDIR
+fi
+
+cd $BINDIR
+
+# Compila la parte "Kinect"
+g++ -std=c++11 -c ../hand-recognizer/Dataset.cpp -o Dataset.o
+g++ -std=c++11 -c ../hand-recognizer/KinectDevice.cpp -o KinectDevice.o
+g++ -std=c++11 -c ../hand-recognizer/InputGenerator.cpp -o InputGenerator.o
+
+# Compila la svm
+g++ -c ../libsvm/svm.cpp -o svm.o
+
+# Compila nell'eseguibile 'hr'
+g++ -std=c++11 -I../hand-recognizer `pkg-config --cflags --libs opencv` *.o ../hand-main.cpp -o hr
