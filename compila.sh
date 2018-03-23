2fed05e0bd3993466169d0526027877dfde59c29
Marco Ortolani <marco.ortolani@gmail.com>
2018-03-23 21:59:01 +0100

    Fix: ref to svm include dir

--- compila.sh
+++ compila.sh
@@ -10,11 +10,11 @@ cd $BINDIR
 
 # Compila la parte "Kinect"
 g++ -std=c++11 -c ../hand-recognizer/Dataset.cpp -o Dataset.o
-g++ -std=c++11 -c ../hand-recognizer/KinectDevice.cpp -o KinectDevice.o
+g++ -std=c++11 -I../libsvm -c ../hand-recognizer/KinectDevice.cpp -o KinectDevice.o
 g++ -std=c++11 -c ../hand-recognizer/InputGenerator.cpp -o InputGenerator.o
 
 # Compila la svm
 g++ -c ../libsvm/svm.cpp -o svm.o
 
 # Compila nell'eseguibile 'hr'
-g++ -std=c++11 -I../hand-recognizer `pkg-config --cflags --libs opencv` *.o ../hand-main.cpp -o hr
+g++ -std=c++11 -I../hand-recognizer -I../libsvm `pkg-config --cflags --libs opencv` *.o ../hand-main.cpp -o hr
